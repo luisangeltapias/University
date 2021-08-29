4ead7c9d8afcc704507ce735115fa45232a9035d
Luis Angel Tapias Madro�ero <luis.tapias.0ds@utap.edu.co>
2021-08-28 22:12:25 -0500

    Agregar archivos de proyecto.

---
+++ University.BL/Data/UniversityContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+using University.BL.Models;
+
+namespace University.BL.Data
+{
+    public class UniversityContext : DbContext
+    {
+        public UniversityContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+    }
+}
